61e04b9585586c2203fb83839f66c9d0c9b73563
Тоболкин Антон Александрович <aatobolkin@ois.ru>
2019-06-11 16:56:34 +0700

    modified test

--- src/test/java/ru/otus/spring/hw01/dao/CsvDaoTest.java
+++ src/test/java/ru/otus/spring/hw01/dao/CsvDaoTest.java
@@ -58,60 +58,10 @@ class CsvDaoTest {
     @Test
     @DisplayName("выдавать корректные задачи из корректного csv-файла")
     void correct_loading_of_the_file() {
-    	//System.out.println(">>> " + limit);
-    	
-        //given(mockLocaleMessageProvider.getMessage("csvfile", null)).willReturn("test_tasks.csv");//"tasks_en.csv"
-
-    	//given(mockLocaleMessageProvider.getMessage(any(), any())).willReturn("test_tasks.csv");
-    	
         TaskDao taskDao = new CsvDao(fakeLocaleMessageProvider);
-
         Queue<Task> queue = taskDao.get();
-        //queue.System.out.println(queue.peek().getQuestion());
-
         BiPredicate<Task, Integer> p = (t, i) -> t.getId() == i && t.getQuestion().equals("Task" + i) && t.getAnswer().equals("Answer" + i);
         assertTrue(IntStream.rangeClosed(1, 5).allMatch(i -> p.test(queue.poll(), i)));
         assertNull(queue.poll());
     }
 }
-
-/*
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = ConfigClass.class)
-@TestPropertySource("classpath:application.properties")
-class CsvDaoTest {
-	
-	@Value("${questions.limit}")
-	private String limit;
-
-	//private static final String TEST_CSV_FILE = "test-tasks.csv";
-	private static final BiPredicate<Task, Integer> NTH_TASK = (task, i) -> task.getId() == i
-			&& task.getQuestion().equals("Task" + i) && task.getAnswer().equals("Answer" + i);
-
-	//@Autowired
-	//@Qualifier("fakeLocaleMessageProvider")
-	private LocaleMessageProvider fakeLocaleMessageProvider;
-	{
-		ReloadableResourceBundleMessageSource ms = new ReloadableResourceBundleMessageSource();
-		ms.setBasename("/i18n/bundle");
-		ms.setDefaultEncoding("UTF-8");
-		fakeLocaleMessageProvider = new FakeLocaleMessageProvider(ms, "en", "US");
-		System.out.println(fakeLocaleMessageProvider);
-	}
-	
-	@Test
-	void correct_loading_of_the_file() {
-		System.out.println(">>> " + limit);
-		
-		TaskDao taskDao = new CsvDao(fakeLocaleMessageProvider);
-
-		Queue<Task> queue = taskDao.get();
-		System.out.println(queue.peek().getQuestion());
-
-		assertTrue(IntStream.rangeClosed(1, 5).allMatch(i -> NTH_TASK.test(queue.poll(), i)));
-		assertNull(queue.poll());
-	}
-	
-
-}
-*/
